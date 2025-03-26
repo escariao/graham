56c85a032069a5371d1a040bd4fc6d0329e21ace
Escariao <42503603+escariao@users.noreply.github.com>
2025-03-26 20:14:27 -0300

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "[BUILD] Atualizando pacotes..."
-apt-get update
-
-echo "[BUILD] Instalando Chromium e ChromeDriver..."
-apt-get install -y chromium chromium-driver
-
-echo "[BUILD] Concluído com sucesso."
